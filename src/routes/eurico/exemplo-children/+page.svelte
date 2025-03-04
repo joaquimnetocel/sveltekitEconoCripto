94c5e6524245becd1e515ae89c753c8a39b852c3
joaquimnetocel <joaquim.neto.cel@gmail.com>
2025-03-04 11:30:39 -0300

    lightweight

---
+++ src/routes/eurico/exemplo-children/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Meucomponente from './Botaoamarelo.svelte';
+</script>
+
+<Meucomponente texto="CLIQUE">
+	<span style="background-color:green;">AQUI</span>
+</Meucomponente>
+
+<Meucomponente texto="APERTE">
+	<span style="background-color:red;">AQUI</span>
+</Meucomponente>
---
+++ src/routes/eurico/exemplo-children/Botaoamarelo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let {
+		children,
+		texto,
+	}: {
+		texto: string;
+		children: Snippet;
+	} = $props();
+</script>
+
+<div>
+	<button
+		style="background:yellow;"
+		onclick={() => {
+			alert('oi');
+		}}
+	>
+		{texto}
+		{@render children()}
+	</button>
+</div>
