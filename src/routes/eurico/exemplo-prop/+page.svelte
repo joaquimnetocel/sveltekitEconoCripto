d9d90c3101baff39b88ab1309ee8945b5348de27
joaquimnetocel <joaquim.neto.cel@gmail.com>
2025-03-04 10:11:57 -0300

    PROP

---
+++ src/routes/eurico/exemplo-prop/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import Meucomponente from './Botaoamarelo.svelte';
+</script>
+
+<Meucomponente />
+<Meucomponente textomenos="MENOS" textomais="MAIS" />
---
+++ src/routes/eurico/exemplo-prop/Botaoamarelo.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let contador = $state(0);
+
+	let {
+		textomais = '+',
+		textomenos = '-',
+	}: {
+		textomais?: string;
+		textomenos?: string;
+	} = $props();
+</script>
+
+<div>
+	<button
+		style="background:yellow;"
+		onclick={() => {
+			contador = contador - 1;
+		}}
+	>
+		{textomenos}
+	</button>
+	{contador}
+	<button
+		style="background:yellow;"
+		onclick={() => {
+			contador = contador + 1;
+		}}
+	>
+		{textomais}
+	</button>
+</div>
