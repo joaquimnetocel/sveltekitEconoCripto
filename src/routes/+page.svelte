825dc80c6934a8c21c01aa97b1e57570f0ea8414
joaquimnetocel <joaquim.neto.cel@gmail.com>
2025-04-15 15:58:20 -0300

    LINKS MELHORADOS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,20 @@
 <ul>
-	<li><a href="./examples/lightweight">LIGHTWEIGHT</a></li>
-	<li><a href="./examples/lightweight-mexc">LIGHTWEIGHT MEXC</a></li>
-	<li><a href="./examples/lightweight-alpaca-cripto">LIGHTWEIGHT ALPACA CRIPTO</a></li>
-	<li><a href="./examples/lightweight-alpaca-acoes">LIGHTWEIGHT ALPACA AÇÕES</a></li>
-</ul>
+	<li>
+		EXEMPLOS:
+		<ul>
+			<li><a href="./examples/lightweight">LIGHTWEIGHT</a></li>
+			<li><a href="./examples/lightweight-mexc">LIGHTWEIGHT MEXC</a></li>
+			<li><a href="./examples/lightweight-alpaca-cripto">LIGHTWEIGHT ALPACA CRIPTO</a></li>
+			<li><a href="./examples/lightweight-alpaca-acoes">LIGHTWEIGHT ALPACA AÇÕES</a></li>
+		</ul>
+	</li>
 
-<ul>
-	<li><a href="./eurico/exemplo-load-function">EXEMPLO FETCH E LOAD FUNCTION</a></li>
-	<li><a href="./eurico/exemplo-state">EXEMPLO STATE</a></li>
-	<li><a href="./eurico/exemplo-componente">EXEMPLO COMPONENTE</a></li>
+	<li>
+		APRENDIZADO
+		<ul>
+			<li><a href="./eurico/exemplo-load-function">EXEMPLO FETCH E LOAD FUNCTION</a></li>
+			<li><a href="./eurico/exemplo-state">EXEMPLO STATE</a></li>
+			<li><a href="./eurico/exemplo-componente">EXEMPLO COMPONENTE</a></li>
+		</ul>
+	</li>
 </ul>
