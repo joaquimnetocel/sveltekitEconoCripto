f757f79ffdbe3ebb7ca4b0bf54284339cf5d5c80
joaquimnetocel <joaquim.neto.cel@gmail.com>
2025-05-24 18:13:26 -0300

    medias moveis

--- src/lib/componentes/LightweightVelas.svelte
+++ src/lib/componentes/LightweightVelas.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
+	import type { typeLinhaLightweight } from '$lib/types/lightweight/typeLinhaLightweight';
 	import type { typeVelaLightWeight } from '$lib/types/lightweight/typeVelaLightWeight';
 	import {
 		CandlestickSeries,
 		createChart,
+		LineSeries,
 		type CandlestickData,
 		type ChartOptions,
 		type DeepPartial,
+		type LineData,
 		type Time,
 	} from 'lightweight-charts';
 	import { untrack } from 'svelte';
 
 	let {
 		velas,
+		linhas = [],
 	}: {
 		velas: typeVelaLightWeight[];
+		linhas?: typeLinhaLightweight[];
 	} = $props();
 
 	let div = $state<HTMLDivElement>();
@@ -34,6 +39,13 @@
 		? R
 		: never;
 
+	let lineSeries: (ReturnType<typeof createChart>['addSeries'] extends (
+		type: infer T,
+		options?: infer O,
+	) => infer R
+		? R
+		: never)[] = [];
+
 	$effect(() => {
 		if (div) {
 			const chart = createChart(div, chartOptions);
@@ -44,6 +56,14 @@
 				wickUpColor: '#26a69a',
 				wickDownColor: '#ef5350',
 			});
+
+			untrack(() => {
+				linhas.forEach((linha, i) => {
+					lineSeries[i] = chart.addSeries(LineSeries, linha.opcoes);
+					lineSeries[i].setData(linha.dados as LineData<Time>[]);
+				});
+			});
+
 			candlestickSeries.setData(untrack(() => velas) as CandlestickData<Time>[]);
 			chart.timeScale().fitContent();
 		}
@@ -51,6 +71,9 @@
 
 	$effect(() => {
 		candlestickSeries.setData(velas as CandlestickData<Time>[]);
+		linhas.forEach((linha, i) => {
+			lineSeries[i].setData(linha.dados as LineData<Time>[]);
+		});
 	});
 </script>
 
--- src/routes/examples/alpaca-lightweight-cripto/Grafico.svelte
+++ src/routes/examples/alpaca-lightweight-cripto/Grafico.svelte
@@ -3,7 +3,9 @@
 	import { funcaoAlpacaParaLightweight } from '$lib/functions/lightweight/funcaoAlpacaParaLightweight';
 	import { funcaoVelasLightWeight } from '$lib/functions/lightweight/funcaoVelasLightweight';
 	import type { typeDadoAlpaca } from '$lib/types/alpaca/typeDadoAlpaca';
+	import type { typeLinhaLightweight } from '$lib/types/lightweight/typeLinhaLightweight';
 	import type { typeVelaLightWeight } from '$lib/types/lightweight/typeVelaLightWeight';
+	import { SMA } from 'technicalindicators';
 
 	let {
 		agora,
@@ -18,13 +20,48 @@
 	} = $props();
 
 	let estadoVelas = $state<typeVelaLightWeight[]>();
+	let linhas = $state<typeLinhaLightweight[]>([]);
 
-	async function funcaoPreencherVelas() {
+	function funcaoCriarMediaMovel(periodo: number): typeLinhaLightweight['dados'] {
+		if (estadoVelas === undefined) return [];
+		const fechamentos = estadoVelas.map((current) => current.close);
+		const mediasmoveis = SMA.calculate({ period: periodo, values: fechamentos });
+		const velasnaoqueimadas = estadoVelas.slice(periodo - 1);
+		return velasnaoqueimadas.map((current, i) => {
+			return {
+				time: current.time,
+				value: mediasmoveis[i],
+			};
+		});
+	}
+
+	function funcaoCalcularMediasMoveis() {
+		if (estadoVelas === undefined) return;
+		linhas[0] = {
+			opcoes: {
+				color: 'red',
+				lineStyle: 2,
+				lineWidth: 2,
+			},
+			dados: funcaoCriarMediaMovel(5),
+		};
+		linhas[1] = {
+			opcoes: {
+				color: 'orange',
+				lineStyle: 2,
+				lineWidth: 2,
+			},
+			dados: funcaoCriarMediaMovel(10),
+		};
+	}
+
+	async function funcaoDados() {
 		const dados_sem_tipagem = await funcaoLerDados();
 		const arrayDadosAlpaca = dados_sem_tipagem as typeDadoAlpaca[];
 		const arrayDadosLightWeight = await funcaoAlpacaParaLightweight(arrayDadosAlpaca);
 		const arrayVelasLightweigth = funcaoVelasLightWeight(arrayDadosLightWeight);
 		estadoVelas = arrayVelasLightweigth;
+		funcaoCalcularMediasMoveis();
 	}
 
 	async function funcaoLerDados() {
@@ -40,23 +77,23 @@
 	$effect(() => {
 		if (periodo === '1Min') {
 			void minuto;
-			funcaoPreencherVelas();
+			funcaoDados();
 			console.log(`ATINGIU PERIODO DE 1 MINUTO`);
 		} else if (periodo === '5Min') {
 			if (minuto % 5 !== 1) return;
-			funcaoPreencherVelas();
+			funcaoDados();
 			console.log('ATINGIU PERIODO DE 5 MINUTOS');
 		} else if (periodo === '15Min') {
 			if (minuto % 15 !== 1) return;
-			funcaoPreencherVelas();
+			funcaoDados();
 			console.log('ATINGIU PERIODO DE 15 MINUTOS');
 		} else if (periodo === '1Hour') {
 			if (minuto !== 1) return;
-			funcaoPreencherVelas();
+			funcaoDados();
 			console.log('ATINGIU PERIODO DE 1 HORA');
 		} else if (periodo === '1Day') {
 			if (hora !== 21 || minuto !== 1) return;
-			funcaoPreencherVelas();
+			funcaoDados();
 			console.log('ATINGIU PERIODO DE 1 HORA');
 		}
 	});
@@ -66,8 +103,8 @@
 	{simbolo} ({periodo})
 </div>
 
-{#await funcaoPreencherVelas()}
+{#await funcaoDados()}
 	<div>CARREGANDO...</div>
 {:then}
-	<LightweightVelas velas={estadoVelas as typeVelaLightWeight[]} />
+	<LightweightVelas velas={estadoVelas as typeVelaLightWeight[]} {linhas} />
 {/await}
